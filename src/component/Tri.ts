cf8da3675cdf700ce46e6fbfad0ca4fc4f94a317
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2023-03-06 22:00:34 +0000

    added tri random gen

--- src/component/Tri.ts
+++ src/component/Tri.ts
@@ -3,25 +3,71 @@ import P5 from 'p5';
 
 export default class Tri {
   _p5: P5;
-  _pos: P5.Vector;
+  _position: P5.Vector;
   _size: number;
+  _style: string;
+  _dir: string | undefined;
+  _colour: string;
 
-  constructor(atPosition: P5.Vector, size: number) {
-    this._p5 = p5;
-    this._pos = atPosition;
+  constructor(
+    position: P5.Vector,
+    size: number,
+    style: string,
+    dir?: string,
+    colour?: string
+  ) {
+    this._position = position;
     this._size = size;
+    this._style = style;
+    this._dir = dir;
+    this._colour = colour ?? 'white';
+
+    this._p5 = p5;
+  }
+
+  drawTri() {
+    const p5 = this._p5;
+    const size = this._size;
+    const centre = size / 2;
+
+    switch (this._style) {
+      case 'square':
+        return p5.square(0, 0, this._size);
+      case 'tri':
+        switch (this._dir) {
+          case 'left':
+            return p5.triangle(0, 0, size, size, 0, size);
+          case 'up':
+            return p5.triangle(0, 0, size, 0, 0, size);
+          case 'right':
+            return p5.triangle(0, 0, size, 0, size, size);
+          case 'down':
+            return p5.triangle(size, 0, size, size, 0, size);
+        }
+      case 'half':
+        switch (this._dir) {
+          case 'left':
+            return p5.triangle(0, 0, centre, centre, 0, size);
+          case 'up':
+            return p5.triangle(0, 0, size, 0, centre, centre);
+          case 'right':
+            return p5.triangle(size, 0, size, size, centre, centre);
+          case 'down':
+            return p5.triangle(size, size, 0, size, centre, centre);
+        }
+      default:
+        return p5.triangle(0, 0, size, 0, size, size);
+    }
   }
 
   draw() {
-    const p5 = this._p5; // just for convenience
+    const p5 = this._p5;
 
     p5.push();
-
-    p5.translate(this._pos);
+    p5.translate(this._position);
     p5.noStroke();
     p5.fill('white');
-    p5.triangle(0, 0, 0, this._size, this._size, this._size);
-
+    this.drawTri();
     p5.pop();
   }
 }
--- src/trigen.ts
+++ src/trigen.ts
@@ -6,8 +6,60 @@ const gridSize = 32;
 const cellsX = Math.floor(window.innerWidth / gridSize);
 const cellsY = Math.floor(window.innerHeight / gridSize);
 
+type TriType = {
+  style: string;
+  dir?: string;
+};
+
+const triOptions: TriType[] = [
+  {
+    style: 'square',
+  },
+  {
+    style: 'tri',
+    dir: 'left',
+  },
+  {
+    style: 'tri',
+    dir: 'up',
+  },
+  {
+    style: 'tri',
+    dir: 'right',
+  },
+  {
+    style: 'tri',
+    dir: 'down',
+  },
+  {
+    style: 'half',
+    dir: 'left',
+  },
+  {
+    style: 'half',
+    dir: 'up',
+  },
+  {
+    style: 'half',
+    dir: 'right',
+  },
+  {
+    style: 'half',
+    dir: 'down',
+  },
+];
+
 const sketch = (p5: p5) => {
-  const myCircles: Tri[] = [];
+  const tris: Tri[] = [];
+
+  const drawTri = (position: p5.Vector, size: number, triIndex: number) => {
+    return new Tri(
+      position,
+      size,
+      triOptions[triIndex].style,
+      triOptions[triIndex].dir
+    );
+  };
 
   p5.setup = () => {
     p5.createCanvas(cellsX * gridSize, cellsY * gridSize).parent(
@@ -20,14 +72,15 @@ const sketch = (p5: p5) => {
 
     for (let x = 0; x < cellsX; x++) {
       for (let y = 0; y < cellsY; y++) {
-        const circlePos = p5.createVector(x * gridSize, y * gridSize);
-        myCircles.push(new Tri(circlePos, gridSize));
+        const pos = p5.createVector(x * gridSize, y * gridSize);
+        const triIndex = Math.floor(Math.random() * triOptions.length);
+        tris.push(drawTri(pos, gridSize, triIndex));
       }
     }
   };
 
   p5.draw = () => {
-    myCircles.forEach((circle) => circle.draw());
+    tris.forEach((circle) => circle.draw());
   };
 };
 
