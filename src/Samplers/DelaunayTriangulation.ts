bbe12eed39897cbab652ee80bfc55c52228fa5c8
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2024-05-17 23:12:15 +0100

    chore: refactor class struct

--- src/Samplers/DelaunayTriangulation.ts
+++ src/Samplers/DelaunayTriangulation.ts
@@ -15,6 +15,7 @@ export class DelaunayTriangulation {
     // pointList is a set of coordinates defining the points to be triangulated
     // triangulation := empty triangle mesh data structure
     // add super-triangle to triangulation // must be large enough to completely contain all the points in pointList
+
     // for each point in pointList do // add all the points one at a time to the triangulation
     //     badTriangles := empty set
     //     for each triangle in triangulation do // first find all the triangles that are no longer valid due to the insertion
@@ -41,33 +42,13 @@ export class DelaunayTriangulation {
   }
 
   private constructTriangleFromIncircle(incenter: Point, radius: number): Triangulation {
-    function getTangentPoint(center: Point, radius: number, angle: number): Point {
-      return {
-        x: center.x + radius * Math.cos(angle),
-        y: center.y - radius * Math.sin(angle),
-      };
-    }
-
-    function intersectionPoint(line1: [number, number, number], line2: [number, number, number]): Point {
-      const [a1, b1, c1] = line1;
-      const [a2, b2, c2] = line2;
-      const determinant = a1 * b2 - a2 * b1;
-
-      if (determinant === 0) throw new Error('Lines do not intersect');
-
-      const x = -(b2 * c1 - b1 * c2) / determinant;
-      const y = -(a1 * c2 - a2 * c1) / determinant;
-
-      return { x, y };
-    }
-
     const angleA = (3 * Math.PI) / 2;
     const angleB = Math.PI / 6;
     const angleC = (5 * Math.PI) / 6;
 
-    const tangentPointA = getTangentPoint(incenter, radius, angleA);
-    const tangentPointB = getTangentPoint(incenter, radius, angleB);
-    const tangentPointC = getTangentPoint(incenter, radius, angleC);
+    const tangentPointA = this.getTangentPoint(incenter, radius, angleA);
+    const tangentPointB = this.getTangentPoint(incenter, radius, angleB);
+    const tangentPointC = this.getTangentPoint(incenter, radius, angleC);
 
     const tangentGradientA = (tangentPointA.y - incenter.y) / (tangentPointA.x - incenter.x);
     const tangentGradientB = (tangentPointB.y - incenter.y) / (tangentPointB.x - incenter.x);
@@ -81,9 +62,9 @@ export class DelaunayTriangulation {
     const lineB: [number, number, number] = [tangentNormalB, -1, tangentPointB.y - tangentNormalB * tangentPointB.x];
     const lineC: [number, number, number] = [tangentNormalC, -1, tangentPointC.y - tangentNormalC * tangentPointC.x];
 
-    const vertexA = intersectionPoint(lineB, lineC);
-    const vertexB = intersectionPoint(lineA, lineC);
-    const vertexC = intersectionPoint(lineA, lineB);
+    const vertexA = this.findIntersectionPoint(lineB, lineC);
+    const vertexB = this.findIntersectionPoint(lineA, lineC);
+    const vertexC = this.findIntersectionPoint(lineA, lineB);
 
     return [vertexA, vertexB, vertexC];
   }
@@ -101,5 +82,25 @@ export class DelaunayTriangulation {
     const midy = (minY + maxY) / 2;
 
     return this.constructTriangleFromIncircle({ x: midx, y: midy }, deltaMax);
+  }
+
+  private findIntersectionPoint(line1: [number, number, number], line2: [number, number, number]): Point {
+    const [a1, b1, c1] = line1;
+    const [a2, b2, c2] = line2;
+    const determinant = a1 * b2 - a2 * b1;
+
+    if (determinant === 0) throw new Error('Lines do not intersect');
+
+    const x = -(b2 * c1 - b1 * c2) / determinant;
+    const y = -(a1 * c2 - a2 * c1) / determinant;
+
+    return { x, y };
+  }
+
+  private getTangentPoint(center: Point, radius: number, angle: number): Point {
+    return {
+      x: center.x + radius * Math.cos(angle),
+      y: center.y - radius * Math.sin(angle),
+    };
   }
 }
